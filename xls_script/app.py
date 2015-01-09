fd6d904d660a782b65e660e46661d135851b20a1
Tomas Pohanka <TomPohys@gmail.com>
2015-01-09 11:45:44 +0100

    All numbers of columns are define as Variable

--- xls_script/app.py
+++ xls_script/app.py
@@ -5,11 +5,19 @@ Created by Tomas Pohanka on 2016-1-5.
 Copyright (c) Tomas Pohanka 2015"""
 
 # Input file from ET
-TIMING = 'RawDataExport_NEDELE2.xls'
+TIMING = 'Hypothesis-s4.xls'
 # Input file with times and names of pictures
 KEYS = 'keys.xls'
 # variable for difference between time in TIMING and KEYS [ms]
 PLUSMINUS = 16
+# EventParam -> for example Key: Down
+EVENT = 18
+# Original time
+TIME = 19
+# GazePosX - bad character
+BADCHAR = 22
+# Header
+HEADER = 7
 
 import xlrd
 import xlwt
@@ -44,11 +52,11 @@ for row in range(sh.nrows):
     # Commentary in TIMING 
     if not sh.row_values(row)[0].startswith("#"):
         # Header
-        if not row == 7:
+        if not row == HEADER:
             # first key down (start)
-            if sh.row_values(row)[18] == "Key: Down" and first_down is False:
-                start_time = sh.row_values(row)[19]
-                new_time = int(sh.row_values(row)[19]) - int(start_time)
+            if sh.row_values(row)[EVENT] == "Key: Down" and first_down is False:
+                start_time = sh.row_values(row)[TIME]
+                new_time = int(sh.row_values(row)[TIME]) - int(start_time)
                 if new_time in keys:
                     name = keys[new_time][0]
                     order = keys[new_time][1]
@@ -56,49 +64,49 @@ for row in range(sh.nrows):
                 # Write the original data
                 for col in range(sh.ncols):
                     # Coortinate containt unexpected value in second and third place and the type is string or unicode
-                    if col == 22 and type(sh.cell(row, col).value) != float:
+                    if col == BADCHAR and type(sh.cell(row, col).value) != float:
                         value = sh.cell(row, col).value
                         value = value.replace(value[1:3], "")
                         write_sh.write(new_row, col, value)
                         continue
                     write_sh.write(new_row, col, sh.cell(row, col).value)
 
-                write_sh.write(new_row, 27, name)
-                write_sh.write(new_row, 28, new_time)
-                write_sh.write(new_row, 29, order)
+                write_sh.write(new_row, sh.ncols, name)
+                write_sh.write(new_row, sh.ncols + 1, new_time)
+                write_sh.write(new_row, sh.ncols + 2, order)
 
                 new_row = new_row + 1
                 continue
 
             # second key down (end)
-            if sh.row_values(row)[18] == "Key: Down" and first_down is True:
-                stop_time = sh.row_values(row)[19]
-                end_time = int(sh.row_values(row)[19]) - int(start_time)
+            if sh.row_values(row)[EVENT] == "Key: Down" and first_down is True:
+                stop_time = sh.row_values(row)[TIME]
+                end_time = int(sh.row_values(row)[TIME]) - int(start_time)
                 first_down = False
                 
                 for col in range(sh.ncols):
-                    if col == 22 and type(sh.cell(row, col).value) != float:
+                    if col == BADCHAR and type(sh.cell(row, col).value) != float:
                         value = sh.cell(row, col).value
                         value = value.replace(value[1:3], "")
                         write_sh.write(new_row, col, value)
                         continue
                     write_sh.write(new_row, col, sh.cell(row, col).value)
 
-                write_sh.write(new_row, 27, name)
-                write_sh.write(new_row, 28, end_time)
-                write_sh.write(new_row, 29, order)
+                write_sh.write(new_row, sh.ncols, name)
+                write_sh.write(new_row, sh.ncols + 1, end_time)
+                write_sh.write(new_row, sh.ncols + 2, order)
 
                 name = ""
                 new_row = new_row + 1
                 continue
 
             # between key down
-            if first_down is True and not sh.row_values(row)[18] == "Key: Down":
+            if first_down is True and not sh.row_values(row)[EVENT] == "Key: Down":
 
-                middle_time = int(sh.row_values(row)[19]) - int(start_time)
+                middle_time = int(sh.row_values(row)[TIME]) - int(start_time)
                 
                 for col in range(sh.ncols):
-                    if col == 22 and type(sh.cell(row, col).value) != float:
+                    if col == BADCHAR and type(sh.cell(row, col).value) != float:
                         value = sh.cell(row, col).value
                         value = value.replace(value[1:3], "")
                         write_sh.write(new_row, col, value)
@@ -108,8 +116,8 @@ for row in range(sh.nrows):
                 if middle_time in keys:
                     name = keys[middle_time][0]
                     order = keys[middle_time][1]
-                    write_sh.write(new_row, 27, name)
-                    write_sh.write(new_row, 29, order)
+                    write_sh.write(new_row, sh.ncols, name)
+                    write_sh.write(new_row, sh.ncols + 2, order)
 
                 else:
                     # Try to find a correct time if time in TIMING and KEYS is not equal
@@ -125,23 +133,23 @@ for row in range(sh.nrows):
                         # Bigger time value in KEYS, smaller in TIMING => set next higher value of time
                         elif (middle_time + i) in keys and (middle_time + i) in keys < middle_time:
                             keys[
-                                (int(sh.row_values(row + 1)[19]) - int(start_time))] = keys[middle_time + i]
+                                (int(sh.row_values(row + 1)[TIME]) - int(start_time))] = keys[middle_time + i]
                             del keys[middle_time + i]
                             continue
                         else:
                             i = i + 1
 
-                        write_sh.write(new_row, 27, name)
-                        write_sh.write(new_row, 29, order)
+                        write_sh.write(new_row, sh.ncols, name)
+                        write_sh.write(new_row, sh.ncols + 2, order)
 
-                write_sh.write(new_row, 28, middle_time)
+                write_sh.write(new_row, sh.ncols + 1, middle_time)
                 new_row = new_row + 1
         else:
             # Write header
             for col in range(sh.ncols):
                 write_sh.write(0, col, sh.cell(row, col).value)
-            write_sh.write(0, 27, "Name")
-            write_sh.write(0, 28, "Time2")
-            write_sh.write(0, 29, "Order")
+            write_sh.write(0, sh.ncols, "Name")
+            write_sh.write(0, sh.ncols + 1, "Time2")
+            write_sh.write(0, sh.ncols + 2, "Order")
 
 write_book_timing.save("new_" + TIMING)
